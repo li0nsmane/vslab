4b8e719dc9438bacc577eaa46403cfe0e480be62
teslatrooper <stefansauerer.210@gmail.com>
2020-01-21 12:08:03 +0100

    Add security stuff to composite-service

---
+++ ContentService/src/main/java/de/hska/ContentService/security/RemoteTokenServiceBeanConfig.java
@@ -0,0 +1,32 @@
+package de.hska.ContentService.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+
+@SuppressWarnings("deprecation")
+@Configuration
+public class RemoteTokenServiceBeanConfig {
+
+    @Bean
+    public AccessTokenConverter accessTokenConverter() {
+        return new DefaultAccessTokenConverter();
+    }
+
+    @Bean
+    public RemoteTokenServices remoteTokenServices(
+            final @Value("${auth.server.url}") String checkTokenUrl,
+            final @Value("${auth.server.clientId}") String clientId,
+            final @Value("${auth.server.clientsecret}") String clientSecret) {
+        final RemoteTokenServices remoteTokenServices = new RemoteTokenServices();
+        remoteTokenServices.setCheckTokenEndpointUrl(checkTokenUrl);
+        remoteTokenServices.setClientId(clientId);
+        remoteTokenServices.setClientSecret(clientSecret);
+        remoteTokenServices.setAccessTokenConverter(accessTokenConverter());
+        return remoteTokenServices;
+    }
+
+}
